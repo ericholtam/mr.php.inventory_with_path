1756f98f1146555742ce02ddc07f8d24f8af5e39
admin <admin@pmunki1dsm.ad.mdp.com>
2020-05-14 10:08:16 -0500

    setup

---
+++ provides.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'listings' => array(
+        'inventory_with_path' => array('view' => 'inventory_with_path', 'i18n' => 'inventory_with_path.inventory_with_path'),
+    ),
+);
